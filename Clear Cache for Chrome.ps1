0fbe05584da64a7e9731cc66e3d6a791391e0a99
Parthiban-sysadmin <119918191+Parthiban-sysadmin@users.noreply.github.com>
2024-02-01 11:45:09 +0530

    Add files via upload

---
+++ Clear Cache for Chrome.ps1
@@ -0,0 +1,29 @@
+﻿# Stop all running Chrome processes
+Stop-Process -Name "chrome" -Force -ErrorAction SilentlyContinue
+
+# Specify the user profile directory
+$userProfilePath = "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Cache"
+
+# Check if the cache directory exists
+if (Test-Path -Path $userProfilePath) {
+    # Get the number of items before clearing the cache
+    $itemsCountBefore = Get-ChildItem -Path $userProfilePath -Recurse | Measure-Object | %{$_.Count}
+
+    # Remove all files in the cache directory
+    Remove-Item -Path $userProfilePath\* -Force -Recurse
+
+    # Get the number of items after clearing the cache
+    $itemsCountAfter = Get-ChildItem -Path $userProfilePath -Recurse | Measure-Object | %{$_.Count}
+
+    # Check if the cache was successfully cleared
+    if ($itemsCountBefore -gt $itemsCountAfter) {
+        Write-Host "Cache cleared for Google Chrome"
+    } else {
+        Write-Host "Failed to clear cache for Google Chrome"
+    }
+} else {
+    Write-Host "Cache directory not found for Google Chrome"
+}
+
+# Start Chrome
+Start-Process "chrome.exe"
---
+++ Clear Cache for edge.ps1
@@ -0,0 +1,29 @@
+﻿
+# Clear cache on Microsoft Edge
+
+function Clear-EdgeCache {
+    param (
+        [string]$username = $env:USERNAME
+    )
+
+    $cachePath = "C:\Users\$username\AppData\Local\Microsoft\Edge\User Data\Default\Cache"
+
+    if (Test-Path -Path $cachePath) {
+        $itemsCountBefore = (Get-ChildItem -Path $cachePath -Recurse | Measure-Object).Count
+
+        Remove-Item -Path $cachePath -Recurse -Force
+
+        $itemsCountAfter = (Get-ChildItem -Path $cachePath -Recurse | Measure-Object).Count
+
+        if ($itemsCountBefore -gt $itemsCountAfter) {
+            Write-Host "Cache cleared for Microsoft Edge"
+        } else {
+            Write-Host "Failed to clear cache for Microsoft Edge"
+        }
+    } else {
+        Write-Host "Specified path doesn't exist."
+    }
+}
+
+# Call the function to clear the cache
+Clear-EdgeCache
